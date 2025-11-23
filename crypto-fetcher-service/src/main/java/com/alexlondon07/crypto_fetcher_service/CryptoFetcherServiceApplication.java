6aeca6abb6253cdbaf59fc8dba63ade6ea2d88c5
Alexander Londoño <alexlondon07@gmail.com>
2025-11-23 18:47:40 -0500

    feat: crypto-fetcher-service @EnableScheduling

--- crypto-fetcher-service/src/main/java/com/alexlondon07/crypto_fetcher_service/CryptoFetcherServiceApplication.java
+++ crypto-fetcher-service/src/main/java/com/alexlondon07/crypto_fetcher_service/CryptoFetcherServiceApplication.java
@@ -2,8 +2,10 @@ package com.alexlondon07.crypto_fetcher_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CryptoFetcherServiceApplication {
 
 	public static void main(String[] args) {
